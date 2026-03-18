65c6a64f3f0b73e58e3e2eb6302b42b10d40eec4
SadieC <sadie.cutts@icloud.com>
2026-03-18 15:40:29 -0500

    Adjusted when saucer will spawn

--- displayManager.js
+++ displayManager.js
@@ -23,7 +23,7 @@ class displayManager{
 
     // checking score to see if we can spawn a saucer 
     canSpawnSaucer(){
-        if(this.score % 240 == 0){
+        if(this.score % 50 == 10){
             return true;
         }
     }
